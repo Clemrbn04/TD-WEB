24d058dd22c589287d52ffd177b4d46daef0da2a
Clemrbn04 <90392685+Clemrbn04@users.noreply.github.com>
2021-10-14 17:05:15 +0200

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,6 @@
+<?php
+     echo 'Prenom'.$_POST["prenom"].'<br>';
+     echo 'Nom'.$_POST["nom"].'<br>';
+     echo 'Email'.$_POST["mail"].'<br>';
+     echo 'Commentaire'.$_POST["commentaire"].'<br>';
+?>
